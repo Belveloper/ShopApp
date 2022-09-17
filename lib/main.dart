e574ef474c1e13d3cf5461884edcf24ad6b3a020
belveloper <belhadj.abderrahmane.co@gmail.com>
2022-09-17 01:22:53 +0100

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('test'),
+        ),
+      ),
+    );
+  }
+}
