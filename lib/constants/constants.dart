671b3a8329628f5db7f1870e4ebe0b9ac581a5f5
belveloper <belhadj.abderrahmane.co@gmail.com>
2022-09-18 01:15:00 +0100

    Added routing method (navigaTo) for better code

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -9,3 +9,7 @@ void navigateToAndFinish(context, widget) {
   Navigator.pushReplacement(
       context, MaterialPageRoute(builder: (context) => widget));
 }
+
+void navigateTo(context, widget) {
+  Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
+}
